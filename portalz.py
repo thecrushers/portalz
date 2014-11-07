5df60edb48ecad38fb1f0b5fd7a9ecd0f31ea0fc
Crusher0001 <thecrusher0001@gmail.com>
2014-11-06 22:07:12 -0500

    Add portal posting endpoints, add/remove/rename some columns

--- portalz.py
+++ portalz.py
@@ -2,6 +2,7 @@ import os
 
 import webapp2
 import jinja2
+import json
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
@@ -25,16 +26,67 @@ class MainPage(webapp2.RequestHandler):
             self.redirect(users.create_login_url(self.request.uri))
 
 
+class SubmitPortal(webapp2.RequestHandler):
+    def post(self):
+        content = json.loads(self.request.body)
+        portal = Portal(
+            guid=content.get('guid'),
+            name=content.get('title'),
+            location=ndb.GeoPt(int(content.get("latE6")) / 1000000., int(content.get("lngE6")) / 1000000.),
+            image=content.get('image'),
+            level=int(content.get('level')),
+            resonators=int(content.get('resCount')),
+            health=content.get('health'),
+            team=content.get('team').lower(),
+        )
+        portal.put()
+        self.response.headers.add('Access-Control-Allow-Origin', 'https://www.ingress.com')
+        self.response.write(portal.guid)
+
+    def options(self):
+        self.response.headers.add('Access-Control-Allow-Origin', 'https://www.ingress.com')
+        self.response.write("OK")
+
+
+class SubmitDetails(webapp2.RequestHandler):
+    def post(self):
+        content = json.loads(self.request.body)
+        portal = Portal(
+            guid=content.get('guid'),
+            name=content.get('title'),
+            location=ndb.GeoPt(int(content.get("latE6")) / 1000000., int(content.get("lngE6")) / 1000000.),
+            image=content.get('image'),
+            level=int(content.get('level')),
+            resonators=int(content.get('resCount')),
+            owner=content.get('owner'),
+            health=content.get('health'),
+            team=content.get('team').lower(),
+        )
+        portal.put()
+        self.response.headers.add('Access-Control-Allow-Origin', 'https://www.ingress.com')
+        self.response.write(portal.guid)
+
+    def options(self):
+        self.response.headers.add('Access-Control-Allow-Origin', 'https://www.ingress.com')
+        self.response.write("OK")
+
+
 class Portal(ndb.Model):
     name        = ndb.StringProperty(indexed=True)
-    description = ndb.TextProperty()
-    owner_uid   = ndb.StringProperty(indexed=True)
-    owner_name  = ndb.StringProperty(indexed=True)
+    guid        = ndb.StringProperty(indexed=True)
+    image       = ndb.StringProperty(indexed=False)
+    owner       = ndb.StringProperty(indexed=True)
     location    = ndb.GeoPtProperty()
+    level       = ndb.IntegerProperty()
+    health      = ndb.IntegerProperty()
+    team        = ndb.StringProperty(indexed=True)
+    resonators  = ndb.IntegerProperty()
     tags        = ndb.StringProperty(repeated=True)
-    captured    = ndb.DateTimeProperty(auto_now_add=False)
+    submitor    = ndb.UserProperty()
     submitted   = ndb.DateTimeProperty(auto_now_add=True)
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/submit_portal', SubmitPortal),
+    ('/submit_details', SubmitDetails)
 ], debug=True)
